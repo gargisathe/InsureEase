ea11ea95be36d354501ffaceb460a43a5828b878
gargisathe <100255101+gargisathe@users.noreply.github.com>
2025-09-16 16:51:50 -0500

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -188,7 +188,7 @@ function ChatPageContent() {
     }
 
     try {
-      const res = await fetch("http://10.16.216.161:8000/query", {
+      const res = await fetch("http://10.0.0.155:8000/query", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ query: fullQuery }),
